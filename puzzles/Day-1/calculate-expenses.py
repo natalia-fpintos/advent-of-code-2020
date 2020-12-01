0c69032e4acad3545ab12a511c557e3457d2795b
Natalia Fernández Pintos <natalia.fpintos@gmail.com>
2020-12-01 22:40:28 +0000

    Day 1(2) - Three sum

--- puzzles/Day-1/calculate-expenses.py
+++ puzzles/Day-1/calculate-expenses.py
@@ -6,9 +6,20 @@ def two_sum(values) -> int:
         sums[2020 - v] = v
 
 
+def three_sum(values) -> int:
+    for i in values:
+        sums = {}
+        for j in values:
+            if j in sums:
+                return j * sums[j][0] * sums[j][1]
+            sums[2020 - j - i] = (j, i)
+
+
 if __name__ == '__main__':
     file_contents = open('input/input.txt', 'r').read().splitlines()
     expenses = [int(v) for v in file_contents]
 
     result = two_sum(expenses)
+    result2 = three_sum(expenses)
     print(result)
+    print(result2)
